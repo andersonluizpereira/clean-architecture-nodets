17d0509350ca3af627591fb25b8d0e1468a1a8df
Anderson Pereira <andersononus@hotmail.com>
2020-05-06 09:21:15 -0300

    feat: ensure emailvalidtor adapter valid email

--- src/utils/email-validator.ts
+++ src/utils/email-validator.ts
@@ -1,6 +1,7 @@
 import { EmailValidator } from '../presentation/protocols/email-validator'
+import validator from 'validator'
 export class EmailValidatorAdapter implements EmailValidator {
-  isValid (email: String): boolean {
-    return false
+  isValid (email: string): boolean {
+    return validator.isEmail(email)
   }
 }
