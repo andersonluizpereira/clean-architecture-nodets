ded337280910e11f9a5f1bf3f898cde342296568
Anderson Pereira <andersononus@hotmail.com>
2020-05-10 19:07:20 -0300

    feat: ensure dbAddAccount returns an account on success

--- src/data/protocols/add-account-repository.ts
+++ src/data/protocols/add-account-repository.ts
@@ -2,5 +2,5 @@ import { AddAccountModel } from '../../domain/usecases/add-account'
 import { AccountModel } from '../../domain/model/account'
 
 export interface AddAccountRepository {
-  async add(accountData: AddAccountModel): Promise<AccountModel>
+  add(accountData: AddAccountModel): Promise<AccountModel>
 }
