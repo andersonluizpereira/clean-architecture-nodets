b8463c238de4a14b963c787f444e1d59a908ff4b
Anderson Pereira <andersononus@hotmail.com>
2020-05-10 20:08:22 -0300

    feat: ensure bycrpadater created make sut throws error

--- src/infra/criptografy/bcrypt-adapter.spec.ts
+++ src/infra/criptografy/bcrypt-adapter.spec.ts
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt'
 import { BycryptAdapter } from './bcrypt-adapter'
+import { rejects } from 'assert'
 
 jest.mock('bcrypt', () => ({
   async hash (): Promise<string> {
@@ -19,10 +20,16 @@ describe('Brypt Adapter', () => {
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
-
   test('Should return a hash on success', async () => {
     const sut = makeSut()
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hash')
+  })
+
+  test('Should throw if bcrypt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
   })
 })
