6cf33915695d35b84a91c40775f4c9cb11a048ef
Anderson Pereira <andersononus@hotmail.com>
2020-05-13 23:03:51 -0300

    feat: ensure cors is enable

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
 import { bodyParse } from '../middlewares/body-parser'
+import { cors } from '../middlewares/cors'
 
 export default (app: Express): void => {
   app.use(bodyParse)
+  app.use(cors)
 }
