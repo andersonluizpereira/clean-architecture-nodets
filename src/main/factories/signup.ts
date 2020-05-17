3c8392deb970819c4017e0523085fc64efca6018
Anderson Pereira <andersononus@hotmail.com>
2020-05-17 20:03:02 -0300

    feat: created log and test

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,14 +1,16 @@
-import { SignUpController } from "../../presentation/controllers/signup/signup"
-import { EmailValidatorAdapter } from "../../utils/email-validator-adapter"
-import { DbAddAccount } from "../../data/usecases/add-account/db-add-account"
-import { BycryptAdapter } from "../../infra/criptografy/bcrypt-adapter"
-import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account"
+import { SignUpController } from '../../presentation/controllers/signup/signup'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+import { BycryptAdapter } from '../../infra/criptografy/bcrypt-adapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { LogControllerDecorator } from '../decorators/log'
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): LogControllerDecorator => {
   const salt = 12
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const bcryptAdapter = new BycryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+  return new LogControllerDecorator(signUpController)
 }
