d28c2b642f1160c118b03ac14d4a567efdd1f11e
Anderson Pereira <andersononus@hotmail.com>
2020-05-20 11:41:43 -0300

    feat: login controller return emails is provided

---
+++ src/presentation/controllers/login/login.ts
@@ -0,0 +1,10 @@
+import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { resolve } from 'dns'
+import { badRequest } from '../../helpers/http-helper'
+import { MissingParamError } from '../../errors'
+
+export class LoginController implements Controller {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
+  }
+}
