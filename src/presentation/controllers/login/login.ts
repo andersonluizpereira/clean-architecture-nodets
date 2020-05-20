2fc945d6856e85327fd0c008c462e4f48a1a745d
Anderson Pereira <andersononus@hotmail.com>
2020-05-20 12:21:46 -0300

    feat: login controller to a method emailvalidator

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,13 @@
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { badRequest } from '../../helpers/http-helper'
 import { MissingParamError } from '../../errors'
+import { EmailValidator } from '../signup/signup-protocols'
 
 export class LoginController implements Controller {
+  constructor (private readonly emailValidator: EmailValidator) {
+
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest.body.email) {
       return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
@@ -10,5 +15,6 @@ export class LoginController implements Controller {
     if (!httpRequest.body.password) {
       return new Promise(resolve => resolve(badRequest(new MissingParamError('password'))))
     }
+    this.emailValidator.isValid(httpRequest.body.email)
   }
 }
