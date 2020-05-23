4e004a7644a3a88742db0abdb0e990dd0bbeb072
Anderson Pereira <andersononus@hotmail.com>
2020-05-23 19:42:50 -0300

    test: ensure RequifieldValidation if validation move makesut

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,14 +1,18 @@
 import { RequiredFieldValidation } from './required-field-validation'
 import { MissingParamError } from '../../errors'
 
+const makeSut = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation('field')
+}
+
 describe('RequiredField Validation', () => {
   test('Should return a MissingParamError if va', () => {
-    const sut = new RequiredFieldValidation('field')
+    const sut = makeSut()
     const error = sut.validate({ name: 'any_name' })
     expect(error).toEqual(new MissingParamError('field'))
   })
   test('Should return if validation succeds', () => {
-    const sut = new RequiredFieldValidation('field')
+    const sut = makeSut()
     const error = sut.validate({ field: 'any_name' })
     expect(error).toBeFalsy()
   })
