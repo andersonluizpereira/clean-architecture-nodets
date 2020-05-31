673e0422d60303621e0d7cfc7efa6c4accaf167c
Anderson Pereira <andersononus@hotmail.com>
2020-05-31 15:18:29 -0300

    feat: ensure Signupcontroller returns 403 if addaccount return null

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -2,7 +2,8 @@ import { SignUpController } from './signup-controller'
 import { MissingParamError, ServerError } from '../../errors'
 import { AccountModel, AddAccount, AddAccountModel, Validation, Authentication, AuthenticationModel } from './signup-controller-protocols'
 import { HttpRequest } from '../../protocols'
-import { ok, serverError, badRequest } from '../../helpers/http/http-helper'
+import { ok, serverError, badRequest, forbidden } from '../../helpers/http/http-helper'
+import { EmailInUseError } from '@/presentation/errors/email-in-use-error'
 
 const makeAuthentication = (): Authentication => {
   class AuthenticationStub implements Authentication {
@@ -87,6 +88,12 @@ describe('SignUp Controller', () => {
     })
   })
 
+  test('Should return 403 if AddAccount returns null', async () => {
+    const { sut, addAccountStub } = makeSut()
+    jest.spyOn(addAccountStub, 'add').mockReturnValueOnce(new Promise(resolve => resolve(null)))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(forbidden(new EmailInUseError()))
+  })
   test('Should return 200 if valid data is provided', async () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle(makeFakeRequest())
--- src/presentation/helpers/http/http-helper.ts
+++ src/presentation/helpers/http/http-helper.ts
@@ -1,8 +1,13 @@
-import { HttpResponse } from '../../protocols/http'
-import { ServerError, UnauthorizedError } from '../../errors'
+import { HttpResponse } from '@/presentation/protocols/http'
+import { ServerError, UnauthorizedError } from '@/presentation/errors'
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
+  body: error
+})
+
+export const forbidden = (error: Error): HttpResponse => ({
+  statusCode: 403,
   body: error
 })
 
@@ -19,4 +24,9 @@ export const serverError = (error: Error): HttpResponse => ({
 export const ok = (data: any): HttpResponse => ({
   statusCode: 200,
   body: data
+})
+
+export const noContent = (): HttpResponse => ({
+  statusCode: 204,
+  body: null
 })
