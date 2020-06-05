f7aa291b22750f3d2d71c7fac6e511b078696d03
Anderson Pereira <andersononus@hotmail.com>
2020-06-05 19:06:57 -0300

    feat: ensure JwtAdapter returns a decrypted values on verify success

--- src/infra/criptografy/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptografy/jwt-adapter/jwt-adapter.spec.ts
@@ -45,5 +45,10 @@ describe('Jwt Adapter', () => {
       await sut.decrypt('any_token')
       expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
     })
+    test('Should return a token on verify success', async () => {
+      const sut = makeSut()
+      const value = await sut.decrypt('any_token')
+      expect(value).toBe('any_value')
+    })
   })
 })
