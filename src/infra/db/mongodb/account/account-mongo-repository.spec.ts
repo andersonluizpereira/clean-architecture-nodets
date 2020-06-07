609a50c3593fcd8ea60c9207afce79c0e1e6af4d
Anderson Pereira <andersononus@hotmail.com>
2020-06-06 21:12:58 -0300

    feat: ensure AccountMongoRepository will not find an account if find by token fails

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -108,10 +108,10 @@ describe('Account Mongo Repository', () => {
       expect(account.password).toBe('any_password')
     })
 
-    // test('Should return null if loadByEmail fails', async () => {
-    //   const sut = makeSut()
-    //   const account = await sut.loadByEmail('any_email@mail.com')
-    //   expect(account).toBeFalsy()
-    // })
+    test('Should return null if loadByToken fails', async () => {
+      const sut = makeSut()
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeFalsy()
+    })
   })
 })
