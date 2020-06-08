3ab64a3b5a4f6653e570d55c6636da8e8a68c9e3
Anderson Pereira <andersononus@hotmail.com>
2020-06-08 20:32:47 -0300

    feat: esnure load surveys route returns 403 if no access token is provided

---
+++ src/main/factories/usecases/survey/load-surveys/db-load-surveys.ts
@@ -0,0 +1,8 @@
+import { SurveyMongoRepository } from '../../../../../infra/db/mongodb/survey/survey-mongo-repository'
+import { LoadSurveys } from '@/domain/usecases/load-surveys'
+import { DbLoadSurveys } from '@/data/usecases/load-surveys/db-load-surveys'
+
+export const makeDbLoadSurveys = (): LoadSurveys => {
+  const surveytMongoRepository = new SurveyMongoRepository()
+  return new DbLoadSurveys(surveytMongoRepository)
+}
