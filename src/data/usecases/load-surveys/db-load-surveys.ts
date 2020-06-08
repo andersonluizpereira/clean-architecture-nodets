bb4bd8a14c55e755808b22c843f05fefc46d35cc
Anderson Pereira <andersononus@hotmail.com>
2020-06-08 08:46:44 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/data/usecases/load-surveys/db-load-surveys.ts
+++ src/data/usecases/load-surveys/db-load-surveys.ts
@@ -1,9 +1,9 @@
 import { LoadSurveys } from '../../../domain/usecases/load-surveys'
-import { LoadSurveyRepository } from '../../../data/protocols/db/survey/load-surveys-repository'
+import { LoadSurveysRepository } from '../../../data/protocols/db/survey/load-surveys-repository'
 import { SurveyModel } from '../../../domain/model/survey'
 
 export class DbLoadSurveys implements LoadSurveys {
-  constructor (private readonly loadSurveyRepository: LoadSurveyRepository) {}
+  constructor (private readonly loadSurveyRepository: LoadSurveysRepository) {}
   async load (): Promise<SurveyModel[]> {
     const surveys = await this.loadSurveyRepository.loadAll()
     return surveys
