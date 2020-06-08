1ed5db3b79cd3e3bb019db2b79dc380b3fc92af2
Anderson Pereira <andersononus@hotmail.com>
2020-06-08 08:45:33 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/data/protocols/db/survey/load-surveys-repository.ts
+++ src/data/protocols/db/survey/load-surveys-repository.ts
@@ -1,5 +1,5 @@
 import { SurveyModel } from '../../../../domain/model/survey'
 
-export interface LoadSurveyRepository {
+export interface LoadSurveysRepository {
   loadAll(): Promise<SurveyModel[]>
 }
