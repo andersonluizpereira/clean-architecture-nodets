8b9ba949d7debff229057d84d777ec848b416672
Anderson Pereira <andersononus@hotmail.com>
2020-06-16 10:34:56 -0300

    refactor: move mockLoadSurveyResultRepositoryStub to a separate file

--- src/data/usecases/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/db-load-survey-result.spec.ts
@@ -1,24 +1,14 @@
-import { SurveyResultModel } from '../survey-result/save-survey-result/db-save-survey-result-protocols'
-import { mockSurveyResultModel } from '@/domain/test'
 import { DbLoadSurveyResult } from './db-load-survey-result'
 import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
+import { mockLoadSurveyResultRepositoryStub } from '@/data/test'
 
 type SutTypes = {
   sut: DbLoadSurveyResult
   loadSurveyResultRepositoryStub: LoadSurveyResultRepository
 }
 
-const makeLoadSurveyResultRepositoryStub = (): LoadSurveyResultRepository => {
-  class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
-    async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
-      return Promise.resolve(mockSurveyResultModel())
-    }
-  }
-  return new LoadSurveyResultRepositoryStub()
-}
-
 const makeSut = (): SutTypes => {
-  const loadSurveyResultRepositoryStub = makeLoadSurveyResultRepositoryStub()
+  const loadSurveyResultRepositoryStub = mockLoadSurveyResultRepositoryStub()
   const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
   return {
     loadSurveyResultRepositoryStub,
